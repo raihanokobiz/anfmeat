154800004c88efe4902658f3ec0854779b4fd499
raihanokobiz <raihan.okobiz@gmail.com>
2025-12-20 13:55:27 +0600

    config

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.anfmeat.com"
+      }
     ],
   },
 };
